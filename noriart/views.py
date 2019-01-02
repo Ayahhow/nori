94a2765f6c9650af9500a21ddb2892acd28452fe
ayahhow <prince_8tears@icloud.com>
2019-01-02 15:57:35 +0900

    heroku upできるかな？

--- noriart/views.py
+++ noriart/views.py
@@ -3,8 +3,9 @@ from noriart import app
 
 @app.route('/')
 def show_firstpage():
-    return render_template('test3.html')
+    return render_template('test.html')
 
 @app.route('/test2')
 def show_test2():
     return render_template('test2.html')
+
